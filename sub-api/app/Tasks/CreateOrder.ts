77d23455d859e5fb1e845f83061ef5e4ce21308a
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-01-04 23:30:19 +0530

    added new table

--- sub-api/app/Tasks/CreateOrder.ts
+++ sub-api/app/Tasks/CreateOrder.ts
@@ -1,11 +1,10 @@
 import { BaseTask } from 'adonis5-scheduler/build'
 import Order from 'App/Models/Order'
 import Subscription from 'App/Models/Subscription'
-
+import User from 'App/Models/User'
 
 // every week of the month
 // every second week of the month
-// 
 
 export default class CreateOrder extends BaseTask {
   public static get schedule() {
@@ -22,14 +21,31 @@ export default class CreateOrder extends BaseTask {
   public async handle() {
     // get all subscriptions
     // create orders
-    const ActiveSubscriptions = await Subscription.query().where('status', 'active')
+    const ActiveSubscriptions = await Subscription.query()
+      .where('status', 'active')
+      .preload('products')
 
     // create Orders
 
-    const newOrders = ActiveSubscriptions.map((el) => {})
+    const userAddress = await User.find(1)
 
-    const createdOrders = Order.createMany({})
+    if (!userAddress) throw new Error('USER NOT FOUND')
 
-    console.log('this is working')
+    ActiveSubscriptions.forEach((sub) => {})
+
+    const createdOrders = await Order.create({
+      address: userAddress?.address,
+    })
+
+    const productObject = {
+      1: {
+        quantity: 1,
+      },
+      2: {
+        quantity: 3,
+      },
+    }
+
+    await createdOrders.related('products').attach(productObject)
   }
 }
