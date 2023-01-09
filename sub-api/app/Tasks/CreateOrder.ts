8c018b2dd729023f176baa9b82bfa2d4c0c03fee
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-01-09 22:20:39 +0530

    added custom days recurrence pattern as well

--- sub-api/app/Tasks/CreateOrder.ts
+++ sub-api/app/Tasks/CreateOrder.ts
@@ -4,7 +4,7 @@ import Schedular from 'App/Models/Schedular'
 import Subscription from 'App/Models/Subscription'
 import OrderService from 'App/Services/OrdersServices'
 import { DateTime } from 'luxon'
-import { addDays, getDay } from 'date-fns'
+import { addDays, format, formatISO, getDay } from 'date-fns'
 
 // every week of the month
 // every second week of the month
@@ -40,66 +40,130 @@ export default class CreateOrder extends BaseTask {
       .preload('days')
 
     ActiveSubscriptions.forEach(async (sub) => {
-      if (sub.recurrence === 'everyday') {
-        let newObject = {}
+      // if (sub.recurrence === 'everyday') {
+      //   let newObject = {}
 
-        const newProductObject = await Promise.all(
-          sub.products.map(async (product) => {
-            const quantitys = await ProductSubscription.query()
-              .where('subscription_id', sub.id)
-              .where('product_id', product.id)
+      //   const newProductObject = await Promise.all(
+      //     sub.products.map(async (product) => {
+      //       const quantitys = await ProductSubscription.query()
+      //         .where('subscription_id', sub.id)
+      //         .where('product_id', product.id)
 
-            const id = product.toJSON().id
+      //       const id = product.toJSON().id
 
-            const jsonProduct = product.toJSON()
+      //       const jsonProduct = product.toJSON()
 
-            console.log('product', product.toJSON())
+      //       console.log('product', product.toJSON())
 
-            console.log('this is quantity', quantitys[0].quantity)
-            newObject[id] = {
-              quantity: quantitys[0].quantity,
-            }
+      //       console.log('this is quantity', quantitys[0].quantity)
+      //       newObject[id] = {
+      //         quantity: quantitys[0].quantity,
+      //       }
 
-            return {
-              ...jsonProduct,
-              quantity: quantitys[0].quantity,
-            }
-          })
-        )
+      //       return {
+      //         ...jsonProduct,
+      //         quantity: quantitys[0].quantity,
+      //       }
+      //     })
+      //   )
 
-        const amount = newProductObject.reduce((prev, curr) => prev + curr.quantity * curr.price, 0)
+      //   const amount = newProductObject.reduce((prev, curr) => prev + curr.quantity * curr.price, 0)
 
-        const newOrders = await OrderService.createOrder({
-          date: '2023-01-04T05:10:20.739Z',
-          amount: amount,
-          quantity: [2, 20],
-          products: newObject,
-        })
+      //   const newOrders = await OrderService.createOrder({
+      //     date: '2023-01-04T05:10:20.739Z',
+      //     amount: amount,
+      //     quantity: [2, 20],
+      //     products: newObject,
+      //   })
 
-        await Schedular.create({
-          createdAt: DateTime.fromISO('2023-01-10T05:10:20.739Z'),
-          updatedAt: DateTime.fromISO('2023-01-10T05:10:20.739Z'),
-          subscriptionId: sub.id,
-          orderId: newOrders.id,
-        })
-      }
+      //   await Schedular.create({
+      //     createdAt: DateTime.fromISO(new Date().toISOString()),
+      //     updatedAt: DateTime.fromISO(new Date().toISOString()),
+      //     subscriptionId: sub.id,
+      //     orderId: newOrders.id,
+      //   })
+      // }
 
-      if (sub.recurrence === 'everyweek') {
+      // if (sub.recurrence === 'everyweek') {
+      //   /**
+      //    * Check if today's day include in days
+      //    * and then make orders
+      //    */
+      //   // test const days = [0, 1, 3, 4, 5, 6]
+
+      //   const days = sub.days.map((el) => el.value)
+
+      //   if (format(new Date(sub.endDate), 'yyyy-MM-dd') === format(new Date(), 'yyyy-MM-dd')) {
+      //     return
+      //   }
+
+      //   const tomorrowDate = addDays(new Date(), 1)
+
+      //   if (days.includes(getDay(tomorrowDate))) {
+      //     // create orders
+      //     let newObject = {}
+
+      //     const newProductObject = await Promise.all(
+      //       sub.products.map(async (product) => {
+      //         const quantitys = await ProductSubscription.query()
+      //           .where('subscription_id', sub.id)
+      //           .where('product_id', product.id)
+
+      //         const id = product.toJSON().id
+
+      //         const jsonProduct = product.toJSON()
+
+      //         console.log('product', product.toJSON())
+
+      //         console.log('this is quantity', quantitys[0].quantity)
+      //         newObject[id] = {
+      //           quantity: quantitys[0].quantity,
+      //         }
+
+      //         return {
+      //           ...jsonProduct,
+      //           quantity: quantitys[0].quantity,
+      //         }
+      //       })
+      //     )
+
+      //     const amount = newProductObject.reduce(
+      //       (prev, curr) => prev + curr.quantity * curr.price,
+      //       0
+      //     )
+
+      //     const newOrders = await OrderService.createOrder({
+      //       date: tomorrowDate.toISOString(),
+      //       amount: amount,
+      //       quantity: [2, 20],
+      //       products: newObject,
+      //     })
+
+      //     await Schedular.create({
+      //       createdAt: DateTime.fromISO(new Date().toISOString()),
+      //       updatedAt: DateTime.fromISO(new Date().toISOString()),
+      //       subscriptionId: sub.id,
+      //       orderId: newOrders.id,
+      //     })
+      //   }
+      // }
+
+      if (sub.recurrence === 'custom') {
         /**
-         * Check if today's day include in days
+         * Check if today's date included in dates
          * and then make orders
          */
-        // test const days = [0, 1, 3, 4, 5, 6]
 
-        const days = sub.days.map((el) => el.value)
+        // test
+        const dates = ['2023-01-10']
 
-        if (sub.endDate === new Date().toString()) {
-          return
-        }
+        // const dates = sub.dates.map((el) => format(new Date(el.date.toString()), 'yyyy-MM-dd'))
 
-        const tomorrowDate = addDays(new Date(), 1)
+        console.log('this are date', dates)
 
-        if (days.includes(getDay(tomorrowDate))) {
+        const tomorrowDate = format(addDays(new Date(), 1), 'yyyy-MM-dd')
+
+        if (dates.includes(tomorrowDate)) {
           // create orders
           let newObject = {}
 
@@ -133,29 +197,19 @@ export default class CreateOrder extends BaseTask {
           )
 
           const newOrders = await OrderService.createOrder({
-            date: tomorrowDate.toISOString(),
+            date: addDays(new Date(), 1).toISOString(),
             amount: amount,
             quantity: [2, 20],
             products: newObject,
           })
 
           await Schedular.create({
-            createdAt: DateTime.fromISO('2023-01-10T05:10:20.739Z'),
-            updatedAt: DateTime.fromISO('2023-01-10T05:10:20.739Z'),
+            createdAt: DateTime.fromISO(new Date().toISOString()),
+            updatedAt: DateTime.fromISO(new Date().toISOString()),
             subscriptionId: sub.id,
             orderId: newOrders.id,
           })
         }
-      }
-
-      if (sub.recurrence === 'custom') {
-        const dates = sub.dates
-        const tomorrowDate = addDays(new Date(), 1)
-
-        /**
-         * Check if today's date included in dates
-         * and then make orders
-         */
       }
     })
   }
